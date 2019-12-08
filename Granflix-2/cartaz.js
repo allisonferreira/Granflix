0d1d5fd83be479cbfe796753fdcce0856bfd6697
allisonferreira <allison.ferreira@bandtec.com.br>
2019-12-08 02:53:13 -0200

    Granflix 2 e subindo ppt da banca

---
+++ Granflix-2/cartaz.js
@@ -0,0 +1,45 @@
+// $('.carousel').carousel();
+
+function apresentar1(){
+   location.href = `./emcartaz/coringa.html`;
+}
+
+function apresentar2(){
+   location.href = `./emcartaz/parcas.html`
+}
+
+function apresentar3(){
+   location.href = `./emcartaz/carcereiros.html`
+}
+
+function apresentar4(){
+   location.href = `./emcartaz/lastchristimas.html`
+}
+
+function apresentar5(){
+   location.href = `./emcartaz/malevola.html`
+}
+
+function apresentar6(){
+   location.href = `./emcartaz/addms.html`
+}
+
+function apresentar7(){
+   location.href = `./emcartaz/dora.html`
+}
+
+function apresentar8(){
+   location.href = `./emcartaz/medoprofundo.html`
+}
+
+function apresentar9(){
+   location.href = `./emcartaz/brooklyn.html`
+}
+
+function apresentar10(){
+   location.href = `./emcartaz/aspanteras.html`
+}
+
+function adicionar(){
+   alert('Filme adicionado ao Carrinho!')
+}
---
+++ Granflix-2/login.js
@@ -0,0 +1,8 @@
+function entrar(){
+if (email.value == 'admin' && senha.value == 'admin'){
+        alert('OK');
+        
+}else{
+    alert('Email ou Senha Invalida');
+}
+}
